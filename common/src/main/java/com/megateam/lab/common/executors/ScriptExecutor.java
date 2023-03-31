421a9bfb860a87e1435e938ea379957450f34a55
Vladislav Andreev <andreevvlad443@gmail.com>
2023-03-31 16:30:11 +0300

    script executor javadoc added

--- common/src/main/java/com/megateam/lab/common/executors/ScriptExecutor.java
+++ common/src/main/java/com/megateam/lab/common/executors/ScriptExecutor.java
@@ -5,18 +5,36 @@ import com.megateam.lab.common.command.util.Exchange;
 import com.megateam.lab.common.exceptions.ExecutionException;
 import com.megateam.lab.common.exceptions.impl.ExectionFailedException;
 import com.megateam.lab.common.exceptions.impl.ScriptIsAlreadyExecutedException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * This class provides execution of several commands at the same time
+ */
 @RequiredArgsConstructor
 public class ScriptExecutor implements Executor {
 
-  @NonNull private Executor executor;
-  public static final Set<String> EXECUTED_SCRIPTS = new HashSet<>();
+  /**
+   * Contains all the executed scripts
+   */
+   public static final Set<String> EXECUTED_SCRIPTS = new HashSet<>();
 
+  /**
+   * Contains a SingleCommandExecutor inside for command execution
+   */
+  @NonNull private Executor executor;
+
+  /**
+   * This method executes commands from the exchanges list
+   * @param script contains an exchanges list to be executed
+   * @return boolean as an execution status
+   * @throws ExecutionException if the group of commands had been already executed
+   * or if some unexpected exception was caught
+   */
   @Override
   public boolean execute(List<Exchange> script) throws ExecutionException {
     for (Exchange commandExchange : script) {
